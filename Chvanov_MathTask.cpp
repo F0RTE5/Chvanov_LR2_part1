a2c9503132e0f587edf3348654a26bc2a303c488
F0RTE5 <alexander.chvanov.jr@gmail.com>
2025-01-29 13:08:27 +0300

    Add main

---
+++ Chvanov_MathTask.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Chvanov_MathTask.h"
+
+using namespace std;
+int main(){
+    string str_input; //вспомогательная переменная
+    //ввод ширины прямоугольника
+    cout << "input height A" << endl;
+    //ввод значения в текстовом виде
+    getline(cin, str_input);
+    //пока ввод некорректен (вводимое значение невозможно преобразовать в int)
+    while (!UserInput(str_input)) {
+        cout << "input height A" << endl;
+        getline(cin, str_input);
+    }
+    //присвоение переменной NumberA преобразованного в тип int
+    //правильно введенного текстового значения
+    int NumberA = stoi(str_input);
+    //ввод высоты прямоугольника
+    cout << "input weight B" << endl;
+    getline(cin, str_input);
+    while (!UserInput(str_input)) {
+        cout << "input weight B" << endl;
+        getline(cin, str_input);
+    }
+    int NumberB = stoi(str_input);
+    //вычисление площади прямоугольника
+    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+    //вывод значения площади
+    cout << "Area of Rectangle is " << RectangleArea << endl;
+}
